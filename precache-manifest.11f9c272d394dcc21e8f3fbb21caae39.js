bfa275d7af6b68e60177a2463158823ea86a927e
Nikunj Sardhara <nsnikunj353@gmail.com>
2020-01-27 13:22:35 +0530

    Updates

---
+++ precache-manifest.11f9c272d394dcc21e8f3fbb21caae39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1071e84ab85b708fb6387fde717ca0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f47788921ed913fe58cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad916442f6bdd5ce6591",
+    "url": "/static/js/2.a43a6e3e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.a43a6e3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f47788921ed913fe58cf",
+    "url": "/static/js/main.ae82f3e5.chunk.js"
+  },
+  {
+    "revision": "a06f17ec189f438409e7",
+    "url": "/static/js/runtime-main.9f5408cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
